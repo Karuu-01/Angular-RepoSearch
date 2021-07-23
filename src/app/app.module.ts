b5f819943c6735930d44450a50743972a888e2de
karuu-01 <brian.muchori@student.moringaschool.com>
2021-07-23 09:22:27 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepoFormComponent } from './repo-form/repo-form.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepoFormComponent
+    RepoFormComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
